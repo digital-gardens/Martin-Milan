80059c2ecb5895af11c6e0a70554647534763494
Martin Milan <martin12333@users.noreply.github.com>
2026-03-07 18:37:13 +0100

    Use .publ.dir suffix for 011-POMOC path
    Update batch script to point to a publishing directory. Changed mkdir (commented) and explorer invocations from %OneDrive%\Dokumenty\011-POMOC to %OneDrive%\Dokumenty\011-POMOC.publ.dir, leaving the original path commented as a fallback. No other logic changes.

--- 011 POMOC NAPOVEDA HELP.f5.cmd
+++ 011 POMOC NAPOVEDA HELP.f5.cmd
@@ -1,8 +1,8 @@
 
 rem 011 POMOC NAPOVEDA HELP.f5.cmd
 
-rem mkdir %OneDrive%\Dokumenty\011-POMOC
-explorer %OneDrive%\Dokumenty\011-POMOC
+rem mkdir %OneDrive%\Dokumenty\011-POMOC.publ.dir
+explorer %OneDrive%\Dokumenty\011-POMOC.publ.dir
 rem explorer "C:\Users\marti\OneDrive\Dokumenty\011-POMOC"
 
 rem pause
