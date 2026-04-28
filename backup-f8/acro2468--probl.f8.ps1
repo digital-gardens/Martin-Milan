9356a127786da90b7343af1d2ddedff759315884
Martin Milan <martin12333@users.noreply.github.com>
2026-04-28 20:25:42 +0200

    Add backup-f8 collection of backup scripts
    Add a set of backup-related notes and helper scripts under backup-f8. New files include acro2468--probl.f8.ps1, acronis.f8.ps1, backup--startup,cp,onedrive,cpio,acronis,github,diigo.f8.sh, experim--mystartup,mymy,cp,cpio,tar.f8.sh, git-clean-WARNING.f8.sh, grant-append-not-write.f8.ps1, second-git-repo.f8.ps1, simulated-non_ecc-ram-error-experiment--acronis.f8.sh, and windows-icacls.f8.ps1. These are f8_text_file style fragments containing commands, troubleshooting notes and small utilities for Acronis, OneDrive, git, ICACLS and backup experiments—primarily documentation and interactive snippets rather than single-run scripts.

---
+++ backup-f8/acro2468--probl.f8.ps1
@@ -0,0 +1,70 @@
+exit
+
+acro
+gui
+maybe i  deleted backups of MILAN 1dr
+while thinking they were of mar 1 dr  ?
+
+MAYBE BECAUSE I **CLONED** THE SETTINGS  ?!
+
+// probably not: MAYBE BECAUSE ONEDRIVE IS NOT A NORMAL FOLDER ... there are ... STRANGE ~~~JUNCTI WHEN VIEWED FROM LINUX .... AND I SHOULD BE LOGGED-IN AS MILAN TO BACKUP MILAN ?!
+// probably not
+
+
+quickfix: set  different  output directories
+still strange in gui cleanup dele
+BOJIM NEPOMOHLO
+restart acro
+nepomohlo
+
+export settings
+C:\Users\marti\OneDrive\acro-sett
+acro-sett--acro2468-prb--before-del-setti-of-milan
+
+
+ctrl-prtscr
+rename the cofig "mila" to "prb 2468"
+
+
+RE-CREATE THE SETTINGS OF MILAN
+del old  tib
+trash recyc
+	///strange full 1.7G should be 2.2
+
+REDELeTE exceptions for allowing tc.exe,  dlljhost to modify tib*
+
+shouldnt have deleted the  config, didnt help anyhow
+
+
+export settings
+C:\Users\marti\OneDrive\acro-sett\acro-sett--acro2468-prb--after-del-setti-of-milan.f8.zip.rename
+
+REDO BKUP OF MARTIN  after editing this file   .ps1
+
+
+
+look inside the 1.7g ... too late
+filelist?
+"C:\Users\marti\OneDrive\backup-f8\divnyMILAN-222-onedri-to-D_inc_b17_s2_v1.tib.txt"
+
+dir C:/Users/milan_rxtipwr/OneDrive/tar-exe
+dir C:/Users/milan_rxtipwr/OneDrive/
+dir C:/Users/marti/OneDrive/tar-exe
+
+"C:\Users\marti\OneDrive\backup-f8\acro2468--probl\2024-06-07_23h10_47.png"
+
+
+web search
+
+strings from gui
+
+	~~clean versions
+	~~clone setti
+
+
+
+
+
+
+	REDELeTE exceptions for allowing tc.exe,  dlljhost to modify tib*
+
