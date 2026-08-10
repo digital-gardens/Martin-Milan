0d9b6d5d9c4047406d987cfdda6bc9196eae06dd
Martin Milan <martin12333@users.noreply.github.com>
2026-08-10 18:31:30 +0200

    msys64\InstallationLog.txt

---
+++ msys64/msys2.f8.ps1
@@ -0,0 +1,368 @@
+
+
+exit
+
+
+
+https://www.gnu.org/software/emacs/manual/html_node/efaq-w32/MinGW_002dw64.html
+
+
+ (eepitch-shell)
+ (eepitch-kill)
+ (eepitch-shell)
+
+
+
+
+cmd.exe
+powershell.exe
+
+exit
+
+
+
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start
+
+ "D:\instu\emacs-28.2\emacs-28.2\bin\runemacs.exe"
+
+
+
+
+
+
+D:\msys64\msys2_shell.cmd -mingw64     -c  "D:/instu/emacs-28.2/emacs-28.2/bin/runemacs.exe"
+#D:\msys64\msys2_shell.cmd -mingw64     -c  "D:\instu\emacs-28.2\emacs-28.2\bin\runemacs.exe"
+
+
+
+
+
+
+#### gui hangs
+D:\msys64\msys2_shell.cmd -mingw64     -c  runemacs
+win-r
+powershell.exe
+kill -Name emacs
+
+
+
+
+
+
+
+
+wget
+which -a wget
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start --help
+
+
+https://www.gnu.org/software/bash/manual/html_node/Invoking-Bash.html
+-c
+Read and execute commands from the first non-option argument command_string, then exit. If there are arguments after the command_string, the first argument is assigned to $0 and any remaining arguments are assigned to the positional parameters. The assignment to $0 sets the name of the shell, which is used in warning and error messages.
+
+
+
+
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start   -c      bash.exe
+
+###D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start         bash.exe
+###/usr/bin/sh: /usr/bin/sh: cannot execute binary file
+
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start     -c        bash
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start    -c     sh
+
+
+#/mingw64/bin/mpv: Bourne-Again shell script
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start          mpv  %*
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start          mpv
+
+#/usr/bin/cmd:            Bourne-Again shell script,
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start          cmd
+
+D:\msys64\msys2_shell.cmd -mingw64    -no-start   -c    emacs
+D:\msys64\msys2_shell.cmd -mingw64    -c  emacs
+D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start   -c  runemacs --help
+
+##D:\msys64\msys2_shell.cmd -mingw64  -defterm  -no-start   -c  runemacs --help
+##/mingw64/bin/emacs: /mingw64/bin/emacs: cannot execute binary file
+
+"C:\Program Files\git\bin\sh.exe" -c "command with arguments"
+
+sh --help
+sh -c "sh --help"
+sh -c "emacs"
+
+
+
+
+
+runemacs
+
+emacs
+
+history -a
+
+win-r
+cmd
+pwsh
+kill -Name emacs
+
+
+
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start
+
+
+-mingw64
+
+
+
+ 0 0 0
+pacman -Ss miniconda
+pacman -Ss conda
+pacman -Ss anaconda
+
+
+pacman -Ss pip
+pacman -Ss python-pip
+python-wheel
+
+
+
+pacman -Ss emacs| grep 64|grep 28
+pacman -Ss emacs| grep 28
+pacman -Ss mpv
+
+pacman -S mingw64/mingw-w64-x86_64-emacs
+28.2-3
+
+Total Download Size:    83.11 MiB
+Total Installed Size:  481.96 MiB
+
+
+
+
+
+
+
+
+
+
+
+
+
+pwd
+cd onedrive
+ls
+
+file `which -a cmd`
+/mingw64/bin/mpv: Bourne-Again shell script
+file `which -a mpv`
+file `which -a sh`
+ls -l `which -a sh.exe`
+ls -l `which -a emacs`
+file `which -a emacs`
+ls -l `which -a runemacs`
+file `which -a runemacs`
+
+/mingw64/bin/emacs: PE32+ executable (console)
+/mingw64/bin/runemacs: PE32+ executable (GUI)
+
+runemacs  --help
+runemacs
+
+win-r
+cmd
+pwsh
+kill -Name emacs
+
+
+"---------------------install-------------------------------------"
+restore point
+pre msys2
+1 year uninstall rollin msys2 win  milan
+
+
+recommend next time:
+non-admin
+and
+i  will try the sfx variant because for the students
+
+
+
+No security vendors and no sandboxes flagged this file as malicious
+https://www.virustotal.com/gui/file/9bc4ee85635f2b3e7b73226c09772b89043da23c7beb4c420f93077b173b293a?nocache=1
+
+https://github.com/martin12333/marti-onedrive/blob/main2/mylen-ila-git-mirmir/OneDrive/AAABKUP-sums20/msys2-base-x86_64-20230127.sfx.exe.sha256
+
+
+
+https://objects.githubusercontent.com/github-production-release-asset-2e65be/80988227/3adee22f-9822-48c8-8fd2-2904ebc8560a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230411%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230411T070140Z&X-Amz-Expires=300&X-Amz-Signature=779014b3e9af335c21d8f52e4d750a6e2b4a317e6d3fafbbea5942c9eaadb5cb&X-Amz-SignedHeaders=host&actor_id=870886&key_id=0&repo_id=80988227&response-content-disposition=attachment%3B%20filename%3Dmsys2-x86_64-20230127.exe&response-content-type=application%2Foctet-stream
+
+cd D:\instu
+bash
+sha256sum ms*
+
+d05c0618027299e9a0834de968596c41bd2012ed8b2534b114a3b9d3fca56809
+
+pwd
+exit
+dir ms*
+cat ms*6
+##& "D:\instu\msys2-x86_64-20230127.exe"
+
+restore point
+"post .\msys2"
+
+
+
+
+icacls.exe D:\msys64
+cmd /c dir /q  D:\msys64
+cmd /c dir /q  D:\msys64\home
+cmd /c dir /q  D:\msys64\etc
+cmd /c dir /q  D:\msys64\usr
+cmd /c dir /q  D:\msys64\usr\share
+
+cmd /c dir /q  D:\msys64\usr\share\cygwin
+
+
+cmd /c dir /q  D:\msys64\
+cmd /c dir /q  D:\msys64\var
+
+exit
+env
+HOMEPATH=\Users\marti
+HOME=/c/Users/marti
+
+cat /etc/passwd
+ls /etc/p*
+
+
+
+
+
+dir D:\msys64\home
+
+D:\msys64
+D:\msys64\msys2_shell.cmd
+D:\msys64\msys2_shell.cmd -msys
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm
+D:\msys64\msys2_shell.cmd -mingw64  -no-start
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start
+exit
+
+D:\msys64\msys2_shell.cmd -mingw64    -conemu
+
+D:\msys64\msys2_shell.cmd --help
+
+-defterm | -mintty | -conemu                            Set terminal type
+
+-here                            Use current directory as working
+								 directory
+-where DIRECTORY                 Use specified DIRECTORY as working
+								 directory
+
+ -no-start                        Do not use "start" command and
+								 return login shell resulting
+								 errorcode as this batch file
+								 resulting errorcode
+
+Any parameter that cannot be treated as valid option and all
+following parameters are passed as login shell command parameters.
+
+
+
+
+set
+env
+
+PATH=/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/c/Windows/System32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0/:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
+
+D:\msys64\msys2_shell.cmd
+D:\msys64\msys2_shell.cmd -mingw64
+
+D:\msys64\msys2_shell.cmd -mingw64 -defterm  -no-start
+
+settings.json:
+{
+    "terminal.integrated.profiles.windows": {
+        "MSYS2 UCRT": {
+            "path": "cmd.exe",
+            "args": [
+                "/c",
+                "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64"
+            ]
+        }
+    }
+}
+
+
+
+
+
+
+
+
+# when installed as admin
+
+pacman -Syy
+error
+error failed to synchronize all databases (unable to lock database)
+
+https://github.com/msys2/MSYS2-packages/issues/2933#issuecomment-1461479234
+Solved this issue by giving my user the Full Control permission over the directory.
+
+# THIS HELPED !
+powershell.exe
+icacls.exe D:\msys64   /grant 'marti:(oi)(ci)(f)'
+exit
+WIN-X ADM
+
+unnecessary worry ... nothing adm
+to check
+Taskmgr.exe
+image path
+D:\msys64
+
+
+
+ls -la /var/lib/pacman/*
+exit
+pslist.exe -t|findstr mintt
+pslist.exe -t
+|findstr mintt
+
+
+
+pacman -Syy
+#pacman -Syu
+
+
+goog
+pacman Syy
+error: mingw64: missing required signature
+error: msys: missing required signature
+error: failed to synchronize all databases (unable to lock database)
+
+#pacman-key --init
+
+#which -a gpg
+
+#update-core
+
+
+
+
+
+exit
+D:\msys64\msys2_shell.cmd -mingw64
+
+
+
+
+
