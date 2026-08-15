0519bcad9a2061a50b8da03d4afe61af81299428
Martin Milan <martin12333@users.noreply.github.com>
2026-08-15 22:57:02 +0200

    - ######

--- 00044-SCRIPTS/GREP--called.f5.sh
+++ 00044-SCRIPTS/GREP--called.f5.sh
@@ -18,7 +18,9 @@ grep         -R -E   -i       --no-filename   '^ *[-+*#]+ .{0,10}[-+*#]+'       
 
 # file:///C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\0004   0111 weigh high    20260813.md
 
-grep         -R -E   -i       '^ *[-+*#]+ .{0,10}[-+*#]+'        "0111--~1"     | sed -E 's/^( *[-] +)[#]{6,}/\1/'  |  sed -E 's/^( *[-] +)(.{0,10}[#]a{5})/\1 ### \2/'  >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep--hhaa---filename.txt.md"
+grep         -R -E   -i       '^ *[-+*#]+ .{0,10}[#][-+*#a]* '        "0111--~1"    >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep--hhaa---filename.txt.md"
+
+#grep         -R -E   -i       '^ *[-+*#]+ .{0,10}[-+*#]+'        "0111--~1"     | sed -E 's/^( *[-] +)[#]{6,}/\1/'  |  sed -E 's/^( *[-] +)(.{0,10}[#]a{5})/\1 ### \2/'  >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep--hhaa---filename.txt.md"
 
 
 
