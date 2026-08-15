3d374856c095ab77bd9b8699ae08cb78b414989d
Martin Milan <martin12333@users.noreply.github.com>
2026-08-15 13:40:06 +0200

    #nn --velke-i ... -I ... hard to debug

--- 00044-SCRIPTS/GREP--called.f5.sh
+++ 00044-SCRIPTS/GREP--called.f5.sh
@@ -5,10 +5,13 @@ echo -- grep --version
 
 #grep --help
 
-# yyyy  --velke-i ... -I   debug: -L
-grep         -R -E   -i      -I   --no-filename        '^ *[-+*#]+ .{0,10}[-+*#]+'  "C:\Users\marti\OneDrive\Dokumenty\0111--songs--music--lyrics----písně--texty"   | sed -E 's/^( *[-] +)[#]{6,}/\1/'  |  sed -E 's/^( *[-] +)(.{0,10}[#]a{5})/\1 ### \2/'  >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep---aaa.txt.md"
+#nn --velke-i ... -I ... hard to debug
 
-sleep 2
+# yyy  grep
+# yy  debug: -L
+grep         -R -E   -i    --no-filename        '^ *[-+*#]+ .{0,10}[-+*#]+'  "C:\Users\marti\OneDrive\Dokumenty\0111--songs--music--lyrics----písně--texty"   | sed -E 's/^( *[-] +)[#]{6,}/\1/'  |  sed -E 's/^( *[-] +)(.{0,10}[#]a{5})/\1 ### \2/'  >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep---aaa.txt.md"
+
+sleep 5
 echo read x
 #read x
 
