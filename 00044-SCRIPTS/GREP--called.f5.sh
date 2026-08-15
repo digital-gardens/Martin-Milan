21298876fca451ad4a6f78e45a25da5f9192e899
Martin Milan <martin12333@users.noreply.github.com>
2026-08-15 16:03:04 +0200

    Update GREP--called.f5.sh

--- 00044-SCRIPTS/GREP--called.f5.sh
+++ 00044-SCRIPTS/GREP--called.f5.sh
@@ -7,9 +7,14 @@ echo -- grep --version
 
 #nn --velke-i ... -I ... hard to debug
 
-# yyy  grep
+cd "C:\Users\marti\OneDrive\Dokumenty"
+
+#ls "0111--~1"
+#ls "0111--songs--music--lyrics----písně--texty"
+
+# yyy  grep    --no-filename   because markdown formatting is broken with filenames in the output
 # yy  debug: -L
-grep         -R -E   -i    --no-filename        '^ *[-+*#]+ .{0,10}[-+*#]+'  "C:\Users\marti\OneDrive\Dokumenty\0111--songs--music--lyrics----písně--texty"   | sed -E 's/^( *[-] +)[#]{6,}/\1/'  |  sed -E 's/^( *[-] +)(.{0,10}[#]a{5})/\1 ### \2/'  >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep---aaa.txt.md"
+grep         -R -E   -i       --no-filename   '^ *[-+*#]+ .{0,10}[-+*#]+'        "0111--~1"     | sed -E 's/^( *[-] +)[#]{6,}/\1/'  |  sed -E 's/^( *[-] +)(.{0,10}[#]a{5})/\1 ### \2/'  >"C:\Users\marti\OneDrive\Dokumenty\00-MM\0111--songs--music--lyrics----písně--texty\grep---aaa.txt.md"
 
 sleep 5
 echo read x
