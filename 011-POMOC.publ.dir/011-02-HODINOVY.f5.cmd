15162ec9970893a94dea6f3cb222ded6da4e8bf4
Martin Milan <martin12333@users.noreply.github.com>
2026-08-25 13:36:23 +0200

    011-02

--- 011-POMOC.publ.dir/011-02-HODINOVY.f5.cmd
+++ 011-POMOC.publ.dir/011-02-HODINOVY.f5.cmd
@@ -3,18 +3,35 @@
 rem  .f5    .cmd
 
 
-rem EDIT NOW THE WINDOWS TASK SCHEDULER CALLS DIRECTLY THE EXPLORER,  NNNOT THE .CMD FILES
+rem #todo %OneDrive%\ in the scheduled task -- is it possible to use %OneDrive% in the scheduled task, or does it have to be the full path, e.g. C:\Users\marti\OneDrive\Dokumenty?
+
+rem ai generated.. do such things happen ?? .. the .cmd file is called, but the .cmd file does not run, because it is in the OneDrive folder, and OneDrive is not running yet
+
+rem edit edit yy ..  NOW THE WINDOWS TASK SCHEDULER CALLS again  THE .CMD FILE
+rem rem nn old EDIT NOW THE WINDOWS TASK SCHEDULER CALLS DIRECTLY THE EXPLORER,  NOT THE .CMD FILES   old
 
 
 rem win q scheduled tasks
 rem f5 to refresh the listing
 rem C:\Users\marti\OneDrive\Dokumenty\00-MM\011-POMOC.publ.dir\011-02-HODINOVY.f5.cmd
 
-rem ? run task as soon as possible after a scheduled start is missed
+rem ? run task as soon as possible after a scheduled start is missed ??
 
 
 
-explorer %OneDrive%\Dokumenty\00-MM\011-POMOC.publ.dir
+
+"C:\Program Files\Git\bin\bash.exe" "C:\Users\marti\OneDrive\Dokumenty\00-MM\00044-SCRIPTS\03-GREP.f5.sh"
+rem C:\Users\marti\OneDrive\Dokumenty\00-MM\00044-SCRIPTS\GREP--called.f5.sh
+
+
+echo on
+echo off
+
+
+rem yy edit
+rem nn explorer %OneDrive%\Dokumenty\00-MM\011-POMOC.publ.dir
+
+
 rem 0820 rem  yy explorer %OneDrive%\Dokumenty\00-MM\011-POMOC.publ.dir
 rem 0819  rem nn explorer C:\Users\marti\OneDrive\Dokumenty\00-MM\011-POMOC.publ.dir
 rem nn 0819
@@ -29,5 +46,7 @@ rem rem explorer "C:\Users\marti\OneDrive\Dokumenty\011-POMOC"
 rem rem rem mkdir %OneDrive%\Dokumenty\011-POMOC.publ.dir
 
 
-rem pause
+echo on
+
+pause
 
