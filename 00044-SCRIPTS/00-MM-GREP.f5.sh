9a299ba75216fe721f1cd126b0d8df61009cb8b6
Martin Milan <martin12333@users.noreply.github.com>
2026-08-26 14:37:04 +0200

    ra rb re

--- 00044-SCRIPTS/00-MM-GREP.f5.sh
+++ 00044-SCRIPTS/00-MM-GREP.f5.sh
@@ -34,7 +34,7 @@ re='[#]a{5}[^a]'
 re='[#]a{6}'
 re='[#]a{5}'
 
-grep -R -E   -i  -m 1     $opf        "$re|$rb"    .   >"C:\Users\marti\OneDrive\Dokumenty\03-20260716-weighted-lists\grep---hhaaaaa.txt.md"
+grep -R -E   -i  -m 1     $opf        "$ra|$rb|$re"    .   >"C:\Users\marti\OneDrive\Dokumenty\03-20260716-weighted-lists\grep---hhaaaaa.txt.md"
 
 head -n 600   "C:\Users\marti\OneDrive\Dokumenty\03-20260716-weighted-lists\grep---hhaaaaa.txt.md"     >"C:\Users\marti\OneDrive\Dokumenty\00-MM\03-20260716-weighted-lists\COPY${opf}---grep---hhaaaaa.txt.md"
 
