6d64168358d5f18696e83b734c1ad2fe5d9d3d57
Martin Milan <martin12333@users.noreply.github.com>
2026-08-30 09:25:41 +0200

    Add PowerShell launcher helper scripts
    Add `a.f8.ps1` in both `00044-SCRIPTS` and `0004cmd` as quick launcher helpers. Each script contains a few `start` usage notes and invokes the existing music robocopy `.cmd` file from PowerShell.

---
+++ 00044-SCRIPTS/a.f8.ps1
@@ -0,0 +1,14 @@
+exit
+
+##start.exe /?
+
+ps1
+
+start explorer.exe /?
+
+# cmd rem # start "" "xyz"
+
+#start
+
+invoke-item "C:\Users\marti\OneDrive\Dokumenty\00-MM\00044-SCRIPTS\0004cmd\0111-music-Robocopy---c-Dokumenty---gee-mir.f5.cmd"
+
